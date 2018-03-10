193916623dce67a86225d87c256dcbd14a0115d9
dhruvil75 <30633522+dhruvil75@users.noreply.github.com>
2018-03-11 02:22:33 +0530

    ...

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ webapp.use(function(err,req,res,next){
 // });
 
 webapp.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode", this.address().port, webapp.settings.env);
   });
