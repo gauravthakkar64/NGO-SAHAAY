9a64fb4fe3c91d05264a5c1dbc397e3ee59f2d14
dhruvil75 <30633522+dhruvil75@users.noreply.github.com>
2018-03-18 10:00:39 +0530

    random certificate number added

--- routes/api.js
+++ routes/api.js
@@ -3,44 +3,71 @@ const express = require('express');
 const router = express.Router();
 const Certificates = require('../models/certificates');
 //get certificates from db
-router.get('/certificates/:id',function (req,res,next) {
-    Certificates.findOne({_id:req.params.id}).then(function(data){
+router.get('/certificates/:id', function (req, res, next) {
+    Certificates.findOne({
+        _id: req.params.id
+    }).then(function (data) {
         res.json(data);
         //res.render("certificate-verify.ejs",{certificate:data});
-        
+
     });
 });
-router.get('/abc',function (req,res,next) {
-        res.send("ABC");
-        //res.render("certificate-verify.ejs",{certificate:data});
+router.get('/test', function (req, res, next) {
+    var id = 123;
+    Certificates.findOne({uniqueId: id}, function(err,obj) { console.log(obj); });
+    res.send("Test Api");
 });
 
 //add new certificates to sb
-router.post('/certificates',function (req,res,next) {
+router.post('/certificates', function (req, res, next) {
     //var certificates = new Certificates(req.body);
     //certificates.save();
-    Certificates.create(req.body).then(function(data,next) {
-        res.send(data);    
-    }).catch(next);
-    //res.send(req.body);
- });
-
- //Update certificates
- router.put('/certificates/:id',function (req,res,next) {
-    Certificates.findByIdAndUpdate({_id:req.params.id},req.body).then(function(){
-        Certificates.findOne({_id:req.params.id}).then(function(data){
-            res.send(data);
+    var flag = true;
+    while (flag) {
+        var id = randomNumber(1000000, 9999999);
+        //console.log(id);
+        Certificates.findOne({uniqueId: id}, function(err,obj) { 
+            console.log("in"); 
+            console.log(obj); 
+            console.log(err);
+            if (obj==null) {
+                flag = false;
+                req.body.uniqueId = id;
+                console.log(req.body.uniqueId);
+                Certificates.create(req.body).then(function (data, next) {
+                    res.send(data);
+                }).catch(next);
+                res.send(req.body);
+            }
         });
-    }); 
-    
- });
+    }
+});
 
- //delete certificates from cb
- router.delete('/certificates/:id',function (req,res,next) {
-    Certificates.findByIdAndRemove({_id:req.params.id}).then(function(data){
-        res.send(data); 
-    });
-    // res.send({type:'DELETE'});
- });
+// //Update certificates
+// router.put('/certificates/:id', function (req, res, next) {
+//     Certificates.findByIdAndUpdate({
+//         _id: req.params.id
+//     }, req.body).then(function () {
+//         Certificates.findOne({
+//             _id: req.params.id
+//         }).then(function (data) {
+//             res.send(data);
+//         });
+//     });
+// });
 
- module.exports = router;
+//delete certificates from db // deletions not allowed
+// router.delete('/certificates/:id', function (req, res, next) {
+//     Certificates.findByIdAndRemove({
+//         _id: req.params.id
+//     }).then(function (data) {
+//         res.send(data);
+//     });
+//     // res.send({type:'DELETE'});
+// });
+
+function randomNumber(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+module.exports = router;
