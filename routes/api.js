fb64c0cca7abd04cc5eea1a7803944a7d08acf77
dhruvil75 <30633522+dhruvil75@users.noreply.github.com>
2018-03-19 07:28:11 +0530

    sync calls done

--- routes/api.js
+++ routes/api.js
@@ -14,39 +14,42 @@ router.get('/certificates/:id', function (req, res, next) {
 });
 router.get('/test', function (req, res, next) {
     var id = 123;
-    Certificates.findOne({uniqueId: id}, function(err,obj) { console.log(obj); });
+    Certificates.findOne({
+        uniqueId: id
+    }, function (err, obj) {
+        console.log(obj);
+    });
     res.send("Test Api");
 });
 
 //add new certificates to sb
+//add new certificates to db
 router.post('/certificates', function (req, res, next) {
-    console.log('in');
     //var certificates = new Certificates(req.body);
     //certificates.save();
-    var flag = true;
-    //while (flag) {
-        var id = randomNumber(1000000, 9999999);
-        //console.log(id);
-        Certificates.findOne({uniqueId: id}, function(err,obj) { 
-            console.log("in"); 
-            console.log(obj); 
-            console.log(err);
-            if (obj==null) {
-                flag = false;
-                req.body.uniqueId = id;
-                console.log(req.body.uniqueId);
-                Certificates.create(req.body).then(function (data, next) {
-                    console.log('sent')
-                    res.send(data);
-                }).catch(next);
-                console.log('done')
-                res.send(req.body);
-            }
+    var flag = false;
+    var i;
+    var id = randomNumber(1000000, 9999999);
+    while (!flag) {
+        Certificates.findOne({
+            uniqueId: id
+        }, function (err, obj) {
+            flag = obj == null;
         });
-    //}
-    console.log('loop')
+        require('deasync').loopWhile(function () {
+            return !flag;
+        });
+    }
+    if (flag) {
+        flag = false;
+        req.body.uniqueId = id;
+        console.log(req.body.uniqueId);
+        Certificates.create(req.body).then(function (data, next) {
+            res.send(data);
+        }).catch(next);
+        res.send(req.body);
+    }
 });
-
 // //Update certificates
 // router.put('/certificates/:id', function (req, res, next) {
 //     Certificates.findByIdAndUpdate({
